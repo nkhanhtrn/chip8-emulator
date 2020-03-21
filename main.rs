014daaf82a654f697c579a796f17ddf876538db6
Khanh Tran <nkhanh.trn@gmail.com>
2020-03-21 16:18:50 +0000

    add load_program function

--- main.rs
+++ main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    let cpu = CPU::new();
+    let mut cpu = CPU::new();
+    let mut program = vec![0x13, 0xC5];
+    cpu.load_program(&mut program);
 }
 
 struct CPU {
     opcode: u16,
-    v: [u8; u16],
+    v: [u8; 16],
     i: u16,
     sound_timer: u8,
     delay_timer: u8,
@@ -24,6 +26,16 @@ impl CPU {
         pc: 0x200,
         sp: 0,
         memory: [0; 4096]
+        }
+    }
+
+    fn load_program(&mut self, program: &mut Vec<u8>) {
+        let mut data = vec![0; 0x200];
+        let mut p = program;
+        data.append(&mut p);
+
+        for (index, &byte) in data.iter().enumerate() {
+            self.memory[index] = byte;
         }
     }
 }
